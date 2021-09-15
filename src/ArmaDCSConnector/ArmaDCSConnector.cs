a407c9feb61c12a7b51b5d3cc4724e2650bc81df
Adrià Alberich <alberichjaumeadria@gmail.com>
2021-09-15 13:23:27 +0200

    initial commit
    added project files
    added folder structure

---
+++ src/ArmaDCSConnector/ArmaDCSConnector.cs
@@ -0,0 +1,142 @@
+﻿using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using RGiesecke.DllExport;
+
+namespace ArmaDCSConnector
+{
+    public class ArmaDCSConnector
+    {
+        private static const int PORT = 3000;
+        private static List<CASEvent> Events;
+
+        /// <summary>
+        /// Gets called when arma starts up and loads all extension.
+        /// It's perfect to load in static objects in a seperate thread so that the extension doesn't needs any seperate initalization
+        /// </summary>
+        /// <param name="output">The string builder object that contains the result of the function</param>
+        /// <param name="outputSize">The maximum size of bytes that can be returned</param>
+#if WIN64
+        [DllExport("RVExtensionVersion", CallingConvention = CallingConvention.Winapi)]
+#else
+        [DllExport("_RVExtensionVersion@8", CallingConvention = CallingConvention.Winapi)]
+#endif
+        public static void RvExtensionVersion(StringBuilder output, int outputSize)
+	    {
+            output.Append("Test-Extension v1.0");
+            Events = new List<CASEvent>();
+            StartListener(PORT);
+        }
+
+        /// <summary>
+        /// The entry point for the default callExtension command.
+        /// </summary>
+        /// <param name="output">The string builder object that contains the result of the function</param>
+        /// <param name="outputSize">The maximum size of bytes that can be returned</param>
+        /// <param name="function">The string argument that is used along with callExtension</param>
+#if WIN64
+        [DllExport("RVExtension", CallingConvention = CallingConvention.Winapi)]
+#else
+        [DllExport("_RVExtension@12", CallingConvention = CallingConvention.Winapi)]
+#endif
+        public static void RvExtension(StringBuilder output, int outputSize, [MarshalAs(UnmanagedType.LPStr)] string function)
+        {
+            output.Append(function);
+        }
+
+        /// <summary>
+        /// The entry point for the callExtensionArgs command.
+        /// </summary>
+        /// <param name="output">The string builder object that contains the result of the function</param>
+        /// <param name="outputSize">The maximum size of bytes that can be returned</param>
+        /// <param name="function">The string argument that is used along with callExtension</param>
+        /// <param name="args">The args passed to callExtension as a string array</param>
+        /// <param name="argsCount">The size of the string array args</param>
+        /// <returns>The result code</returns>
+#if WIN64
+        [DllExport("RVExtensionArgs", CallingConvention = CallingConvention.Winapi)]
+#else
+        [DllExport("_RVExtensionArgs@20", CallingConvention = CallingConvention.Winapi)]
+#endif
+        public static int RvExtensionArgs(StringBuilder output, int outputSize, [MarshalAs(UnmanagedType.LPStr)] string function, [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.LPStr, SizeParamIndex = 4)] string[] args, int argCount)
+	    {
+            foreach(var arg in args)
+            {
+                output.Append(arg);
+            }
+
+            return 0;
+        }
+
+        private static void StartListener(int port)
+        {
+            TcpListener server=null;
+            try
+            {
+                // Set the TcpListener on localhost
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localAddr, port);
+
+                // Start listening for client requests.
+                server.Start();
+
+                // Buffer for reading data
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                // Enter the listening loop.
+                while(true)
+                {
+                    Console.Write("Waiting for a connection... "); //replace
+
+                    // Perform a blocking call to accept requests.
+                    // You could also use server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    data = null;
+
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    // Loop to receive all the data sent by the client.
+                    while((i = stream.Read(bytes, 0, bytes.Length))!=0)
+                    {
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Received: {0}", data);
+
+                        // Process the data sent by the client.
+                        data = data.ToUpper();
+                        
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                        // Send back a response.
+                        stream.Write(msg, 0, msg.Length);
+                        Console.WriteLine("Sent: {0}", data);
+                    }
+
+                    // Shutdown and end connection
+                    client.Close();
+                }
+            }
+            catch(SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+            }
+        }
+    }
+}
